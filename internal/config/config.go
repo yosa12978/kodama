84f41dfb307116180010f9a9e1819bfa36793361
yosa12978 <yosa12978@gmail.com>
2025-05-31 23:42:34 +0300

    improved postgres config options

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io"
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -14,10 +15,12 @@ type Config struct {
 		Addr     string `yaml:"addr"`
 	} `yaml:"app"`
 	Postgres struct {
-		Database string `yaml:"database"`
-		Addr     string `yaml:"addr"`
-		User     string `yaml:"user"`
-		Password string `yaml:"password"`
+		Database     string `yaml:"database"`
+		Addr         string `yaml:"addr"`
+		User         string `yaml:"user"`
+		UserFile     string `yaml:"user_file"`
+		Password     string `yaml:"password"`
+		PasswordFile string `yaml:"password_file"`
 	} `yaml:"postgres"`
 }
 
@@ -31,15 +34,19 @@ func DefaultConfig() Config {
 			Addr:     "0.0.0.0:80",
 		},
 		Postgres: struct {
-			Database string "yaml:\"database\""
-			Addr     string "yaml:\"addr\""
-			User     string "yaml:\"user\""
-			Password string "yaml:\"password\""
+			Database     string "yaml:\"database\""
+			Addr         string "yaml:\"addr\""
+			User         string "yaml:\"user\""
+			UserFile     string "yaml:\"user_file\""
+			Password     string "yaml:\"password\""
+			PasswordFile string "yaml:\"password_file\""
 		}{
-			Database: "kodama_db",
-			Addr:     "localhost:5432",
-			User:     "postgres",
-			Password: "postgres",
+			Database:     "kodama_db",
+			Addr:         "localhost:5432",
+			User:         "postgres",
+			UserFile:     "",
+			Password:     "postgres",
+			PasswordFile: "",
 		},
 	}
 }
@@ -55,5 +62,29 @@ func ReadConfig(filename string) (Config, error) {
 	if errors.Is(err, io.EOF) {
 		return conf, nil
 	}
+	if err := parseFileConfigs(&conf); err != nil {
+		return conf, err
+	}
 	return conf, err
+}
+
+func parseFileConfigs(c *Config) error {
+
+	if c.Postgres.UserFile != "" {
+		userFile, err := os.ReadFile(c.Postgres.UserFile)
+		if err != nil {
+			return err
+		}
+		c.Postgres.User = strings.TrimSpace(string(userFile))
+	}
+
+	if c.Postgres.PasswordFile != "" {
+		passwordFile, err := os.ReadFile(c.Postgres.PasswordFile)
+		if err != nil {
+			return err
+		}
+		c.Postgres.Password = strings.TrimSpace(string(passwordFile))
+	}
+
+	return nil
 }
